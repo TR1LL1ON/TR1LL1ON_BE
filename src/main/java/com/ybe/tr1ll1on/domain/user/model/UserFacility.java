a40dca949a50c1c863a1f4234feb75fd93dc55bd
Su-daa <subin.kk019@gmail.com>
2023-11-23 12:13:47 +0900

    Feat: UserFacility 추가
    유저별 개인화를 위한 개인 편의시설 여부 추가

---
+++ src/main/java/com/ybe/tr1ll1on/domain/user/model/UserFacility.java
@@ -0,0 +1,21 @@
+package com.ybe.tr1ll1on.domain.user.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class UserFacility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_facility_id")
+    private Long id;
+    private Boolean cooking;
+    private Boolean parking;
+    private Boolean sports;
+    private Boolean sauna;
+    private Boolean beauty;
+}
