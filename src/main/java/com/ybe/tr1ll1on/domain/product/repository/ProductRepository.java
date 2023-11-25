b024466f0dff0ec08d332b8a666f8edb5215bf76
sungjiwoon <nebulaisme@naver.com>
2023-11-26 03:33:59 +0900

    feat : ProductRepository 생성

--- src/main/java/com/ybe/tr1ll1on/domain/product/repository/ProductRepository.java
+++ src/main/java/com/ybe/tr1ll1on/domain/product/repository/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.ybe.tr1ll1on.domain.product.repository;
 
 import com.ybe.tr1ll1on.domain.product.model.Product;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    List<Product> findByAccommodationIdAndStandardNumberLessThanEqual(Long accommodationId, Integer personNumber);
 }
