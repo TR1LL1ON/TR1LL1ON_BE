7ef21779be03505a3ba3b26236464444449dd361
sungjiwoon <nebulaisme@naver.com>
2023-11-26 03:34:31 +0900

    feat : ProductController 작성

--- src/main/java/com/ybe/tr1ll1on/domain/product/controller/ProductController.java
+++ src/main/java/com/ybe/tr1ll1on/domain/product/controller/ProductController.java
@@ -1,10 +1,15 @@
 package com.ybe.tr1ll1on.domain.product.controller;
 
-import com.ybe.tr1ll1on.domain.product.dto.AccomodationRequest;
+import com.ybe.tr1ll1on.domain.product.dto.AccommodationDetailResponse;
+import com.ybe.tr1ll1on.domain.product.dto.AccommodationRequest;
 import com.ybe.tr1ll1on.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,11 +23,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProductController {
     private final ProductService productService;
 
-//    @PostMapping("/")
-//    public ResponseEntity<Void> getProduct(
-//        @RequestParam(name = "accomodation_id") final Long accomodationId,
-//        @RequestBody final AccomodationRequest accomodationRequest
-//    ) {
-//
-//    }
+    @PostMapping("/{accommodation_id}")
+    public ResponseEntity<AccommodationDetailResponse> getProductList(
+        @PathVariable("accommodation_id") final Long accommodationId,
+        @RequestBody final AccommodationRequest accommodationRequest
+    ) {
+        log.info("HÎ11" + accommodationId+", "+ accommodationRequest.getPersonNumber());
+        return ResponseEntity.ok(
+                productService.getProduct(accommodationId, accommodationRequest)
+        );
+    }
 }
