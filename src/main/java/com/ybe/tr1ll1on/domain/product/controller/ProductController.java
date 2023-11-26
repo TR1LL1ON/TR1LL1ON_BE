1c332e34a652014af4bfbc51bb4a9da2854143b0
sungjiwoon <nebulaisme@naver.com>
2023-11-26 15:42:54 +0900

    feat : 객실 상세 조회 컨트롤러

--- src/main/java/com/ybe/tr1ll1on/domain/product/controller/ProductController.java
+++ src/main/java/com/ybe/tr1ll1on/domain/product/controller/ProductController.java
@@ -4,20 +4,16 @@ import static com.ybe.tr1ll1on.domain.product.error.ProductExceptionCode.CHECKIN
 
 import com.ybe.tr1ll1on.domain.product.dto.AccommodationDetailResponse;
 import com.ybe.tr1ll1on.domain.product.dto.AccommodationRequest;
+import com.ybe.tr1ll1on.domain.product.dto.ProductResponse;
 import com.ybe.tr1ll1on.domain.product.error.ProductException;
-import com.ybe.tr1ll1on.domain.product.error.ProductExceptionCode;
 import com.ybe.tr1ll1on.domain.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -32,13 +28,27 @@ public class ProductController {
         @PathVariable("accommodation_id") final Long accommodationId,
         @RequestBody final AccommodationRequest accommodationRequest
     ) {
-        log.info("HÎ11" + accommodationId+", "+ accommodationRequest.getPersonNumber());
         //TODO checkIn 과 checkOut은 같은 날짜이면 안됨!!
         if (accommodationRequest.getCheckIn().isEqual(accommodationRequest.getCheckOut())) {
             throw new ProductException(CHECKIN_EQUALS_CHECKOUT);
         }
         return ResponseEntity.ok(
-                productService.getProduct(accommodationId, accommodationRequest)
+                productService.getAccommodationDetail(accommodationId, accommodationRequest)
+        );
+    }
+
+    @PostMapping("/{accommodation_id}/{product_id}")
+    public ResponseEntity<ProductResponse> getProductDetail(
+            @PathVariable("accommodation_id") final Long accommodationId,
+            @PathVariable("product_id") final Long product_id,
+            @RequestBody final AccommodationRequest accommodationRequest
+    ) {
+        //TODO checkIn 과 checkOut은 같은 날짜이면 안됨!!
+        if (accommodationRequest.getCheckIn().isEqual(accommodationRequest.getCheckOut())) {
+            throw new ProductException(CHECKIN_EQUALS_CHECKOUT);
+        }
+        return ResponseEntity.ok(
+                productService.getProduct(accommodationId, product_id, accommodationRequest)
         );
     }
 }
