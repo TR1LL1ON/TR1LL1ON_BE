a67cee861ea24af9d93a8cffe8d0ed0dd8b00f7e
Su-daa <subin.kk019@gmail.com>
2023-11-27 23:13:32 +0900

    Feat: 사용안하는 builder 어노테이션 삭제

--- src/main/java/com/ybe/tr1ll1on/domain/accommodation/dto/AccommodationRequestDTO.java
+++ src/main/java/com/ybe/tr1ll1on/domain/accommodation/dto/AccommodationRequestDTO.java
@@ -10,7 +10,6 @@ import java.time.LocalDate;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 public class AccommodationRequestDTO {
     @NotNull
     LocalDate checkIn;
--- src/main/java/com/ybe/tr1ll1on/domain/accommodation/dto/AccommodationResponseDTO.java
+++ src/main/java/com/ybe/tr1ll1on/domain/accommodation/dto/AccommodationResponseDTO.java
@@ -1,6 +1,5 @@
 package com.ybe.tr1ll1on.domain.accommodation.dto;
 
-import com.ybe.tr1ll1on.domain.accommodation.model.AccommodationImage;
 import lombok.*;
 
 
@@ -17,18 +16,4 @@ public class AccommodationResponseDTO {
     private String areaCode;
     private Integer price;
     private Double score;
-
-    public AccommodationResponseDTO(Long accommodationId, String imageUrl, String name, String address, String areaCode, Integer price) {
-        this.accommodationId = accommodationId;
-        this.imageUrl = imageUrl==null ? AccommodationImage.BASIC_ACCOMMODATION_IMG : imageUrl;
-        this.name = name;
-        this.address = address;
-        this.areaCode = areaCode;
-        this.price = price;
-    }
-
-    public AccommodationResponseDTO(Long accommodationId, String name) {
-        this.accommodationId = accommodationId;
-        this.name = name;
-    }
 }
