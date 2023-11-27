ec045d52690386eb593d5d805eb004c71d35c91d
sungjiwoon <nebulaisme@naver.com>
2023-11-27 15:22:21 +0900

    fix : test 수정

--- src/test/java/com/ybe/tr1ll1on/Tr1ll1onApplicationTests.java
+++ src/test/java/com/ybe/tr1ll1on/Tr1ll1onApplicationTests.java
@@ -3,11 +3,11 @@ package com.ybe.tr1ll1on;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class Tr1ll1onApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class Tr1ll1onApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
