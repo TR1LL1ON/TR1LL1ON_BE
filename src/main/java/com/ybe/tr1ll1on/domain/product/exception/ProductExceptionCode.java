5be56cb9222debc68915f85f67f3f8917c71a9f5
sungjiwoon <nebulaisme@naver.com>
2023-11-27 13:28:01 +0900

    Refactor : 체크인, 체크아웃 검증할 DateUtil 생성 및 처리

--- src/main/java/com/ybe/tr1ll1on/domain/product/exception/ProductExceptionCode.java
+++ src/main/java/com/ybe/tr1ll1on/domain/product/exception/ProductExceptionCode.java
@@ -10,7 +10,6 @@ import org.springframework.http.HttpStatus;
 public enum ProductExceptionCode implements ExceptionCode {
 
     EMPTY_PRODUCT(HttpStatus.NOT_FOUND, "EMPTY_PRODUCT", "상품 정보를 가져오지 못했습니다."),
-    CHECKIN_EQUALS_CHECKOUT(HttpStatus.BAD_REQUEST, "CHECKIN_EQUALS_CHECKOUT", "체크인 날짜와 체크아웃 날짜가 동일합니다.")
     ;
 
     private final HttpStatus status;
