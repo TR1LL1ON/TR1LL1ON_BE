24123e297b6cee820d3a2b2a280c69ff0b2245d3
sungjiwoon <nebulaisme@naver.com>
2023-11-28 14:50:16 +0900

    fix : entity OrderItem, Orders

--- src/main/java/com/ybe/tr1ll1on/domain/order/model/OrderItem.java
+++ src/main/java/com/ybe/tr1ll1on/domain/order/model/OrderItem.java
@@ -3,6 +3,8 @@ package com.ybe.tr1ll1on.domain.order.model;
 import com.ybe.tr1ll1on.domain.product.model.Product;
 import com.ybe.tr1ll1on.domain.review.model.Review;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,6 +12,8 @@ import java.time.LocalDate;
 
 @Entity
 @NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Getter
 public class OrderItem {
     @Id
@@ -18,11 +22,14 @@ public class OrderItem {
     private Long id;
 
     private LocalDate startDate;
+
     private LocalDate endDate;
+
     private Integer personNumber;
+
     private Integer price;
 
-    private boolean reviewWritten;
+    private Boolean reviewWritten;
 
     @OneToOne(mappedBy = "orderItem", cascade = CascadeType.REMOVE)
     private Review review;
