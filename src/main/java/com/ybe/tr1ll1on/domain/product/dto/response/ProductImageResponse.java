e2c11c0fafe67471f8fe32b56f5fc30d392be93d
sungjiwoon <nebulaisme@naver.com>
2023-11-28 14:52:09 +0900

    fix : 스네이크 -> 카멜 케이스

--- src/main/java/com/ybe/tr1ll1on/domain/product/dto/response/ProductImageResponse.java
+++ src/main/java/com/ybe/tr1ll1on/domain/product/dto/response/ProductImageResponse.java
@@ -1,7 +1,5 @@
-package com.ybe.tr1ll1on.domain.product.response;
+package com.ybe.tr1ll1on.domain.product.dto.response;
 
-import com.fasterxml.jackson.databind.PropertyNamingStrategies;
-import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import com.ybe.tr1ll1on.domain.product.model.ProductImage;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
