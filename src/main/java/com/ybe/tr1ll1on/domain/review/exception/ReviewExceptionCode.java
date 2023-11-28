662634cb4d657833218a347f88d21051053ddf81
yurim0628 <dbfla0303@naver.com>
2023-11-28 11:42:55 +0900

    Feat: 마이페이지 (주문 정보 및 주문 상세 정보 ) 기능 구현

--- src/main/java/com/ybe/tr1ll1on/domain/review/exception/ReviewExceptionCode.java
+++ src/main/java/com/ybe/tr1ll1on/domain/review/exception/ReviewExceptionCode.java
@@ -14,10 +14,7 @@ public enum ReviewExceptionCode implements ExceptionCode {
 
     // 리뷰 관련 예외
     REVIEW_NOT_FOUND(HttpStatus.NOT_FOUND, "404 Not Found", "해당 리뷰를 찾을 수 없습니다."),
-    REVIEW_CONFLICT(HttpStatus.CONFLICT, "409 Conflict", "이미 존재하는 리뷰 입니다."),
-
-    // 주문 아이템 관련 예외
-    ORDER_ITEM_NOT_FOUND(HttpStatus.NOT_FOUND, "404 Not Found", "주문 상품을 찾을 수 없습니다.");
+    REVIEW_CONFLICT(HttpStatus.CONFLICT, "409 Conflict", "이미 존재하는 리뷰 입니다.");
 
     private final HttpStatus status;
     private final String code;
---
+++ src/main/java/com/ybe/tr1ll1on/domain/user/dto/response/MyPageListResponse.java
@@ -0,0 +1,32 @@
+package com.ybe.tr1ll1on.domain.user.dto.response;
+
+import com.ybe.tr1ll1on.domain.order.model.Orders;
+import com.ybe.tr1ll1on.global.common.Payment;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MyPageListResponse {
+    private Long orderId;
+    private Long AccommodationId;
+    private Date orderCreateDate;
+    private Payment payment;
+    private Integer totalPrice;
+
+    public static MyPageListResponse fromEntity(Orders order) {
+        return MyPageListResponse.builder()
+                .orderId(order.getId())
+                .AccommodationId(order.getOrderItemList().get(0).getProduct().getAccommodation().getId())
+                .orderCreateDate(order.getOrderCreateDate())
+                .payment(order.getPayment())
+                .totalPrice(order.getTotalPrice())
+                .build();
+    }
+}
