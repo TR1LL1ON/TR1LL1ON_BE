c0d98e8482997a62d0ae979fc01d5167a8b9e422
sungjiwoon <nebulaisme@naver.com>
2023-11-28 14:48:25 +0900

    fix : ProductController Valid 추가

--- src/main/java/com/ybe/tr1ll1on/domain/product/controller/ProductController.java
+++ src/main/java/com/ybe/tr1ll1on/domain/product/controller/ProductController.java
@@ -1,11 +1,12 @@
 package com.ybe.tr1ll1on.domain.product.controller;
 
 
-import com.ybe.tr1ll1on.domain.product.response.AccommodationDetailResponse;
-import com.ybe.tr1ll1on.domain.product.request.AccommodationRequest;
-import com.ybe.tr1ll1on.domain.product.response.ProductResponse;
+import com.ybe.tr1ll1on.domain.product.dto.response.AccommodationDetailResponse;
+import com.ybe.tr1ll1on.domain.product.dto.request.AccommodationRequest;
+import com.ybe.tr1ll1on.domain.product.dto.response.ProductResponse;
 import com.ybe.tr1ll1on.domain.product.service.ProductService;
 import com.ybe.tr1ll1on.global.date.utill.DateUtil;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
@@ -25,7 +26,7 @@ public class ProductController {
     @PostMapping("/{accommodation_id}")
     public ResponseEntity<AccommodationDetailResponse> getAccommodationDetail(
         @PathVariable("accommodation_id") final Long accommodationId,
-        @RequestBody final AccommodationRequest accommodationRequest
+        @Valid @RequestBody final AccommodationRequest accommodationRequest
     ) {
 //        TODO checkIn 과 checkOut은 같은 날짜이면 안됨!!
         DateUtil.isValidCheckInBetweenCheckOut(
@@ -40,7 +41,7 @@ public class ProductController {
     public ResponseEntity<ProductResponse> getProductDetail(
             @PathVariable("accommodation_id") final Long accommodationId,
             @PathVariable("product_id") final Long product_id,
-            @RequestBody final AccommodationRequest accommodationRequest
+            @Valid @RequestBody final AccommodationRequest accommodationRequest
     ) {
         //TODO checkIn 과 checkOut은 같은 날짜이면 안됨!!
         DateUtil.isValidCheckInBetweenCheckOut(
