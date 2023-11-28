5853296d9fccf54e08e0a17b620d357b5869de30
Su-daa <subin.kk019@gmail.com>
2023-11-28 13:39:00 +0900

    Refactor: dto 패키지 request, response 패키지로 세분화

--- src/main/java/com/ybe/tr1ll1on/domain/accommodation/repository/AccommodationMapper.java
+++ src/main/java/com/ybe/tr1ll1on/domain/accommodation/repository/AccommodationMapper.java
@@ -1,7 +1,7 @@
 package com.ybe.tr1ll1on.domain.accommodation.repository;
 
-import com.ybe.tr1ll1on.domain.accommodation.dto.AccommodationRequestDTO;
-import com.ybe.tr1ll1on.domain.accommodation.dto.AccommodationResponseDTO;
+import com.ybe.tr1ll1on.domain.accommodation.dto.request.AccommodationRequestDTO;
+import com.ybe.tr1ll1on.domain.accommodation.dto.response.AccommodationResponseDTO;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
