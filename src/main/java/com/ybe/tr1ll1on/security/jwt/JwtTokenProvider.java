75792a5aff81b77491d0a093f53b053abac91798
yurim0628 <dbfla0303@naver.com>
2023-11-29 11:18:36 +0900

    Fix: cors 설정

--- src/main/java/com/ybe/tr1ll1on/security/jwt/JwtTokenProvider.java
+++ src/main/java/com/ybe/tr1ll1on/security/jwt/JwtTokenProvider.java
@@ -89,14 +89,14 @@ public class JwtTokenProvider {
 
     // Response Cookie를 생성하여 Refresh Token을 저장한다.
     private void storeRefreshTokenInCookie(HttpServletResponse response, String refreshToken) {
-        Cookie cookie = new Cookie("refreshToken", refreshToken);
-        
-        cookie.setMaxAge(7 * 24 * 60 * 60);
-        cookie.setHttpOnly(true);
-        cookie.setSecure(true);
-//        cookie.setDomain(".tr1ll1on.site");
-
-        response.addCookie(cookie);
+        ResponseCookie cookie = ResponseCookie.from(REFRESH_TOKEN, refreshToken)
+                .httpOnly(true)     // 1. JavaScript에서 쿠키에 접근을 막기 위해 httpOnly 설정
+                .secure(true)       // 2. HTTPS에서만 쿠키 전송을 허용
+                .path("/auth")      // 3. 쿠키가 전송될 수 있는 경로 설정
+                .sameSite("None")    // 4. 동일 사이트와 크로스 사이트에 모두 쿠키 전송이 가능
+                .build();
+        // Response Header에도 Cookie를 저장한다.
+        response.addHeader("Set-Cookie", cookie.toString());
     }
 
     /**
