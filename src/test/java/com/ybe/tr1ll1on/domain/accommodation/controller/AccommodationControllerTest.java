38d32322a718688db1b87c91426c672f88341a8a
Su-daa <subin.kk019@gmail.com>
2023-11-30 20:47:31 +0900

    Test: 숙소 조회 controller 테스트 코드 작성

--- src/test/java/com/ybe/tr1ll1on/domain/accommodation/controller/AccommodationControllerTest.java
+++ src/test/java/com/ybe/tr1ll1on/domain/accommodation/controller/AccommodationControllerTest.java
@@ -1,35 +1,23 @@
 package com.ybe.tr1ll1on.domain.accommodation.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ybe.tr1ll1on.domain.accommodation.dto.request.AccommodationRequest;
 import com.ybe.tr1ll1on.domain.accommodation.dto.response.AccommodationResponse;
-import com.ybe.tr1ll1on.domain.accommodation.model.Accommodation;
 import com.ybe.tr1ll1on.domain.accommodation.service.AccommodationService;
-import com.ybe.tr1ll1on.global.date.exception.InValidDateException;
-import com.ybe.tr1ll1on.global.date.util.DateUtil;
+import jakarta.servlet.ServletException;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import java.time.LocalDate;
 import java.util.Collections;
 
-import static com.ybe.tr1ll1on.global.date.exception.InValidDateExceptionCode.CHECKIN_EQUALS_CHECKOUT;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
@@ -44,7 +32,6 @@ class AccommodationControllerTest {
     private AccommodationService accommodationService;
 
     private MockMvc mockMvc;
-    private ObjectMapper objectMapper;
 
     @BeforeEach
     public void setUp() {
@@ -79,30 +66,29 @@ class AccommodationControllerTest {
                 .andExpect(status().isOk());
     }
 
-//    @Test
-//    @DisplayName("숙소 전체 조회 컨트롤러 실패 테스트")
-//    public void getAll_실패() throws Exception {
-//
-//        // Mock 데이터 설정
-//        LocalDate checkInDate = LocalDate.now();
-//        LocalDate checkOutDate = LocalDate.now();
-//
-//        AccommodationRequest accommodationRequest = new AccommodationRequest();
-//        accommodationRequest.setCheckIn(checkInDate);
-//        accommodationRequest.setCheckOut(checkOutDate);
-//        accommodationRequest.setPersonNumber(2);
-//
-//        AccommodationResponse accommodationResponse = new AccommodationResponse();
-//
-//        given(accommodationService.findAccommodation(accommodationRequest))
-//                .willReturn(Collections.singletonList(accommodationResponse));
-//
-//        mockMvc.perform(get("/products")
-//                        .param("checkIn", String.valueOf(accommodationRequest.getCheckIn()))
-//                        .param("checkOut", String.valueOf(accommodationRequest.getCheckOut()))
-//                        .param("personNumber", String.valueOf(accommodationRequest.getPersonNumber()))
-//                        .contentType(MediaType.APPLICATION_JSON))
-//                .andDo(print());
-//
-//    }
+    @Test
+    @DisplayName("숙소 전체 조회 컨트롤러 실패 테스트")
+    public void getAll_실패() throws Exception {
+
+        // Mock 데이터 설정
+        LocalDate checkInDate = LocalDate.now();
+        LocalDate checkOutDate = LocalDate.now();
+
+        AccommodationRequest accommodationRequest = new AccommodationRequest();
+        accommodationRequest.setCheckIn(checkInDate);
+        accommodationRequest.setCheckOut(checkOutDate);
+        accommodationRequest.setPersonNumber(2);
+
+        AccommodationResponse accommodationResponse = new AccommodationResponse();
+
+        given(accommodationService.findAccommodation(accommodationRequest))
+                .willReturn(Collections.singletonList(accommodationResponse));
+
+        Assertions.assertThatThrownBy(()-> mockMvc.perform(get("/products")
+                        .param("checkIn", String.valueOf(accommodationRequest.getCheckIn()))
+                        .param("checkOut", String.valueOf(accommodationRequest.getCheckOut()))
+                        .param("personNumber", String.valueOf(accommodationRequest.getPersonNumber()))
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andReturn()).isInstanceOf(ServletException.class);
+    }
 }
