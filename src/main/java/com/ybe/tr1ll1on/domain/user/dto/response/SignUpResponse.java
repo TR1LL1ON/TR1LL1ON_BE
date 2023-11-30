90ff6391b06df9ee3cf56c8ee4dc67eb5dff01e5
WestSilver99 <seo606101@naver.com>
2023-11-30 10:18:24 +0900

    Docs : 스웨거 문서작업 AuthController 리프레쉬 토큰, 로그아웃, 로그인 resquest, response body 못적음

--- src/main/java/com/ybe/tr1ll1on/domain/user/dto/response/SignUpResponse.java
+++ src/main/java/com/ybe/tr1ll1on/domain/user/dto/response/SignUpResponse.java
@@ -1,5 +1,6 @@
 package com.ybe.tr1ll1on.domain.user.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -10,7 +11,12 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Builder
 public class SignUpResponse {
+    @Schema(example = "1")
+
     private Long id;
+    @Schema(example = "test@eamil.com")
     private String email;
+    @Schema(example = "테스트")
+
     private String name;
 }
