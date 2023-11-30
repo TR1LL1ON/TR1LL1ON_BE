1210eadf83348d70f135995da23016edbc86d7ca
WestSilver99 <seo606101@naver.com>
2023-11-30 18:36:00 +0900

    Test : 장바구니 테스트 서비스(미완)

--- src/test/java/com/ybe/tr1ll1on/domain/cart/service/CartServiceImplTest.java
+++ src/test/java/com/ybe/tr1ll1on/domain/cart/service/CartServiceImplTest.java
@@ -1,33 +1,34 @@
 package com.ybe.tr1ll1on.domain.cart.service;
 
-import com.ybe.tr1ll1on.domain.cart.dto.request.AddCartItemRequest;
-import com.ybe.tr1ll1on.domain.cart.dto.response.AddCartItemResponse;
-import org.springframework.security.core.Authentication;
+import com.ybe.tr1ll1on.domain.cart.dto.response.CartResponse;
 import com.ybe.tr1ll1on.domain.cart.model.Cart;
 import com.ybe.tr1ll1on.domain.cart.model.CartItem;
 import com.ybe.tr1ll1on.domain.cart.repository.CartItemRepository;
 import com.ybe.tr1ll1on.domain.cart.repository.CartRepository;
 import com.ybe.tr1ll1on.domain.product.model.Product;
+import com.ybe.tr1ll1on.domain.product.model.ProductImage;
 import com.ybe.tr1ll1on.domain.product.repository.ProductRepository;
 import com.ybe.tr1ll1on.domain.user.model.User;
 import com.ybe.tr1ll1on.domain.user.repository.UserRepository;
-import com.ybe.tr1ll1on.security.util.SecurityUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.time.LocalDate;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class CartServiceImplTest {
 
     @InjectMocks
@@ -47,90 +48,75 @@ public class CartServiceImplTest {
 
     @BeforeEach
     public void setUp() {
-        MockitoAnnotations.openMocks(this);
+        // 새로운 SecurityContext를 생성하고 설정
+        SecurityContext securityContextMock = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        when(securityContextMock.getAuthentication()).thenReturn(authentication);
+
+        // Creating a principal object directly
+        org.springframework.security.core.userdetails.User principalUser =
+                new org.springframework.security.core.userdetails.User(
+                        "testUser", "testPassword", Collections.emptyList());
+        when(authentication.getPrincipal()).thenReturn(principalUser);
+
+        // 설정한 SecurityContext를 SecurityContextHolder에 넣어줍니다.
+        SecurityContextHolder.setContext(securityContextMock);
     }
 
     @Test
-    public void addCartItemTest() {
-        // 테스트에 필요한 더미 데이터 설정
-        Long productId = 1L;
-        Long userId = 2L;
-        Long cartId = 3L;
-
-        AddCartItemRequest request = new AddCartItemRequest();
-        request.setProductId(productId);
-        request.setCheckIn(LocalDate.now());
-        request.setCheckOut(LocalDate.now().plusDays(1));
-        request.setPersonNumber(2);
-        request.setPrice(20000);
-
-        Product product = Product.builder()
-                .id(productId)
-                .name("Sample Product")
-                .checkInTime("14:00")
-                .checkOutTime("12:00")
-                .standardNumber(1)
-                .maximumNumber(5)
-                .count(10)
-                .build();
-
-        // User 객체 생성 및 필요한 데이터 초기화
-        User user = User.builder()
-                .id(userId)
-                .cart(Cart.builder().id(cartId).user(user).build())
-                .build();
-
-        Cart cart = new Cart();
-        cart.setId(cartId);
-        cart.setUser(user);
-
-        SecurityContext securityContext = SecurityContextHolder.getContext();
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-        // Mock 설정
-        when(productRepository.findById(productId)).thenReturn(Optional.of(product));
-        when(securityContext.getAuthentication()).thenReturn(authentication);
-        when(authentication.getPrincipal()).thenReturn(userId);
-        when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        when(user.getCart()).thenReturn(cart);
-        when(cartItemRepository.save(any())).thenAnswer(invocation -> {
-            CartItem savedCartItem = invocation.getArgument(0);
-            savedCartItem.setId(4L); // Mocked ID
-            return savedCartItem;
-        });
-
-        // 테스트 실행
-        AddCartItemResponse result = cartService.addCartItem(request);
-
-        // 결과 검증
-        assertEquals(productId, result.getProductId());
-        assertEquals(request.getPersonNumber(), result.getPersonNumber());
-
-    }
-
-
-    @Test
-    public void removeCartItemTest() {
+    void getAllCarts() {
         // 테스트에 필요한 더미 데이터 설정
         Long userId = 1L;
-        Long cartItemIdToRemove = 2L;
+        Long cartId = 2L;
 
+        SecurityContext securityContextMock = mock(SecurityContext.class);
+        Authentication authentication = mock(Authentication.class);
+        when(securityContextMock.getAuthentication()).thenReturn(authentication);
+
+        // Creating a principal object directly
+        org.springframework.security.core.userdetails.User principalUser =
+                new org.springframework.security.core.userdetails.User(
+                        "testUser", "testPassword", Collections.emptyList());
+        when(authentication.getPrincipal()).thenReturn(principalUser);
+
+        // 설정한 SecurityContext를 SecurityContextHolder에 넣어줍니다.
+        SecurityContextHolder.setContext(securityContextMock);
+
+
+        // User 객체 생성 및 필요한 데이터 초기화
         User user = new User();
-        user.setId(userId);
-
-        CartItem cartItem = new CartItem();
-        cartItem.setId(cartItemIdToRemove);
-        cartItem.setCart(new Cart());
-        cartItem.getCart().setUser(user);
 
         // Mock 설정
-        when(SecurityUtil.getCurrentUserId()).thenReturn(userId);
         when(userRepository.findById(userId)).thenReturn(Optional.of(user));
-        when(cartItemRepository.findById(cartItemIdToRemove)).thenReturn(Optional.of(cartItem));
+
+        // cart 객체를 mock으로 만들기
+        Cart cart = mock(Cart.class);
+
+        // cartRepository.findById(cartId) 메서드를 호출할 때 mock cart 객체 반환하도록 설정
+        when(cartRepository.findById(cartId)).thenReturn(Optional.of(cart));
+
+        // cart.getCartItem() 메서드를 호출할 때 원하는 값을 반환하도록 설정
+        CartItem cartItem = mock(CartItem.class);
+        when(cart.getCartItem()).thenReturn(Collections.singletonList(cartItem));
+
+        // cartItem 객체의 getProduct() 메서드를 호출할 때 원하는 값을 반환하도록 설정
+        Product product = mock(Product.class);
+        when(cartItem.getProduct()).thenReturn(product);
+
+        // product.getProductImageList() 메서드를 호출할 때 원하는 값을 반환하도록 설정
+        ProductImage productImage = new ProductImage();
+        when(product.getProductImageList()).thenReturn(Collections.singletonList(productImage));
 
         // 테스트 실행
-        cartService.removeCartItem(cartItemIdToRemove);
+        List<CartResponse> result = cartService.getAllCarts();
 
         // 결과 검증
-        verify(cartItemRepository, times(1)).deleteById(cartItemIdToRemove);
+        assertEquals(1, result.size());
+        CartResponse cartResponse = result.get(0);
+        assertEquals(cartItem.getId(), cartResponse.getCartItemId());
+        assertEquals(product.getAccommodation().getId(), cartResponse.getAccommodationId());
+        assertEquals(product.getId(), cartResponse.getProductId());
+        assertEquals(product.getAccommodation().getName(), cartResponse.getAccommodationName());
+        assertEquals(product.getAccommodation().getAddress(), cartResponse.getAccommodationAddress());
     }
 }
