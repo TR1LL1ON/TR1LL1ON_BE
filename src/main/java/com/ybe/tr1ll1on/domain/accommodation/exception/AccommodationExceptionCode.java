bb72eccf29960698da531f55c0fb28c2b6a3472f
Su-daa <subin.kk019@gmail.com>
2023-12-04 20:28:27 +0900

    Feat: 인원수 관련 에러코드 추가

--- src/main/java/com/ybe/tr1ll1on/domain/accommodation/exception/AccommodationExceptionCode.java
+++ src/main/java/com/ybe/tr1ll1on/domain/accommodation/exception/AccommodationExceptionCode.java
@@ -10,8 +10,8 @@ import org.springframework.http.HttpStatus;
 public enum AccommodationExceptionCode implements ExceptionCode {
 
     // 숙소 관련 예외
-    ACCOMMODATION_NOT_FOUND(HttpStatus.NOT_FOUND, "404 Not Found", "해당 숙소를 찾을 수 없습니다")
-    ;
+    ACCOMMODATION_NOT_FOUND(HttpStatus.NOT_FOUND, "404 Not Found", "해당 숙소를 찾을 수 없습니다"),
+    INVALID_PERSON_NUMBER(HttpStatus.BAD_REQUEST, "400 Bad Request", "인원 수는 1명 이상이어야 합니다");
 
     private final HttpStatus status;
     private final String code;
