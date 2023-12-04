347ea0cf84108d902819fcc79d329e05e1996acf
sungjiwoon <nebulaisme@naver.com>
2023-12-04 13:00:07 +0900

    refactor : AccommodationException 예외 처리 리팩토링

--- src/main/java/com/ybe/tr1ll1on/domain/review/exception/AccommodationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.ybe.tr1ll1on.domain.review.exception;
-
-import com.ybe.tr1ll1on.global.exception.ExceptionCode;
-import com.ybe.tr1ll1on.global.exception.TrillionException;
-
-public class AccommodationNotFoundException extends TrillionException {
-    public AccommodationNotFoundException (ExceptionCode errorCode) { super(errorCode); }
-}
